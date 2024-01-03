27075d163c7ffcb8cd2b33bb668538fea5aa65e4
lack <phuss.pro@gmail.com>
2024-01-03 09:10:11 +0100

    firebase config

--- src/firebase.ts
+++ src/firebase.ts
@@ -3,12 +3,15 @@ import { initializeApp } from "firebase/app";
 import { getAuth } from "firebase/auth";
 import { getFirestore } from "firebase/firestore";
 import { getStorage } from "firebase/storage";
+import dotenv from 'dotenv';
+dotenv.config();
+
 // TODO: Add SDKs for Firebase products that you want to use
 // https://firebase.google.com/docs/web/setup#available-libraries
 
 // Your web app's Firebase configuration
 const firebaseConfig = {
-  apiKey: "AIzaSyBSV_g8xfLrFmjR1Co3wddLievSBuQyOs8",
+  apiKey: process.env.REACT_APP_FIREBASE_API_KEY,
   authDomain: "react-chat-85afe.firebaseapp.com",
   projectId: "react-chat-85afe",
   storageBucket: "react-chat-85afe.appspot.com",
